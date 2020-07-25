d4b3c11a05eb559dd464d11625d1198928c61ba7
kihyo9 <kihyo9@gmail.com>
2020-07-25 09:59:27 -0400

    initial python code

---
+++ RecursiveSudokuSolver.py
@@ -0,0 +1,16 @@
+def possible(y,x,n):
+	global grid
+	for i in range(0,9):
+		if grid[y][i] == n:
+			return False
+	for i in range(0,9):
+		if grid[i][x] == n:
+			return False
+	x0 = (x//3)*3
+	y0 = (y//3)*3
+
+	for i in range(0,3):
+			for j in range(0,3):
+				if grid[y0+1][x0+j] == n:
+					return False
+	return True
