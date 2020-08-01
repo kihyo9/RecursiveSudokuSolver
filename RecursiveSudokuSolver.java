a5ee8817dc56b1c8f730f75d1edcd924bd8598b7
Elise <50882635+elisecho@users.noreply.github.com>
2020-08-01 10:20:05 -0400

    Adding Comments

--- RecursiveSudokuSolver.java
+++ RecursiveSudokuSolver.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class RecursiveSudokuSolver {
-    static Scanner sc = new Scanner(System.in); 
+    static Scanner sc = new Scanner(System.in);
+    //create a grid with unfinished sudoku puzzle
     static int [][] grid = {
         {5, 0, 0, 0, 8, 0, 0, 4, 9},
         {0, 0, 0, 5, 0, 0, 0, 3, 0},
@@ -17,12 +18,12 @@ public class RecursiveSudokuSolver {
     static void solve(){
       for(int y = 0; y < 9; y++){
          for(int x = 0; x < 9; x++){
-            if(grid[y][x] == 0){
+            if(grid[y][x] == 0){ //check if there is a 0 at this position
                for(int n = 1; n < 10; n++){
-                  if(possible(y,x,n)){
+                  if(possible(y,x,n)){ //calls the possible method
                      grid[y][x] = n;
-                     solve();
-                     grid[y][x] = 0;
+                     solve(); //recursively call the method we are using
+                     grid[y][x] = 0; //this is called "backtracking", we reset the solution to 0
                   }
                }
                return;
@@ -30,23 +31,26 @@ public class RecursiveSudokuSolver {
          }
       }
       printGrid();
-      System.out.println("More?");
+      System.out.println("More?"); //here we ask the user to check for another possible solution
       String inp = sc.nextLine();
     }
-    
+    //y is the column
+    //x is the row
+    //n is the number that goes into the square
     static boolean possible(int y, int x, int n){
+        //first we solve for rows and columns
       for(int i = 0; i < 9; i++){
          if(grid[y][i] == n || grid[i][x] == n){
             return false;
          }
       }
-      
+      //next we solve for the 3 X 3 squares
       int x0 = (x/3)*3;
       int y0 = (y/3)*3;
       
       for(int i = 0; i < 3; i++){
          for(int j = 0; j < 3; j++){
-            if(grid[y0+i][x0+j] == n){
+            if(grid[y0+i][x0+j] == n){ //compare n value to values within square
                return false;
             }
          }
